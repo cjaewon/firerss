5fcbfefed03d9af26d394b4b0d4664b70a6ae2c4
cjaewon <jwn4492@outlook.com>
2020-09-10 21:43:37 +0900

    Create octokit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+async function main() {
+
+}
+
+main();
---
+++ src/lib/octokit.ts
@@ -0,0 +1,5 @@
+import { Octokit } from "@octokit/rest";
+
+const octokit = new Octokit({ auth: `token ${process.env.GH_TOKEN}` });
+
+export default octokit;
